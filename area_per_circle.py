3a3a64da3ec6a248e03dfed61ee16a76420a4a43
Ubuntu <ubuntu@ip-172-31-85-213.ec2.internal>
2022-03-04 03:20:58 +0000

    Area and perimeter of a circle

---
+++ area_per_circle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created By: Ferdaws
+
+# Date: 3/3/2022
+
+# This program shows the area and perimeter of a circle with radius of 15mm.
+
+
+import math
+
+
+def main():
+
+    print("A circle with a radius ")
+    print("of 15 mm:")
+    print("")
+    print("Area = {} mm^2". format(math.pi*15**2))
+    print("Perimeter = {} mm". format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+  main()
